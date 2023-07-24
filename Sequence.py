a0666e427d09ec68a7071e70aa7426e152a7fb2e
Carlos Oliva <olivam@protonmail.com>
2023-07-23 18:35:26 -0700

    Eliminé variables de prueba innecesarias en Sequence

--- Sequence.py
+++ Sequence.py
@@ -14,41 +14,7 @@ AMINOACIDS = 'ACDEFGHIKLMNPQRSTVWY'
 
 
 
-AMINOACIDS_BY_CATEGORY = {
-  'negative': 'DE',
-  'positive': 'HKR',
-  'unchargedPolar': 'NQSTY',
-  'nonpolar': 'ACFGILMPVW'
-}
-
-
-
-AMINOACID_CATEGORIES = {
-  'A': 'nonpolar',
-  'C': 'nonpolar',
-  'D': 'negative',
-  'E': 'negative',
-  'F': 'nonpolar',
-  'G': 'nonpolar',
-  'H': 'positive',
-  'I': 'nonpolar',
-  'K': 'positive',
-  'L': 'nonpolar',
-  'M': 'nonpolar',
-  'N': 'unchargedPolar',
-  'P': 'nonpolar',
-  'Q': 'unchargedPolar',
-  'R': 'positive',
-  'S': 'unchargedPolar',
-  'T': 'unchargedPolar',
-  'V': 'nonpolar',
-  'W': 'nonpolar',
-  'Y': 'unchargedPolar'
-}
-
-
-
-TEST_SEQUENCE = 'GREETINGSHEREAREELEVENSIDECHAINNAMES' + \
+_TEST_SEQUENCE = 'GREETINGSHEREAREELEVENSIDECHAINNAMES' + \
   'ALANINECYSTEINEASPARTATEPHENYLALANINEGLYCINEHISTIDINELYSINEASPARAGINE' + \
   'ARGININESERINEVALINE'
 
