a9634e96e9921429d8bd1109345ad4664f57025f
Carlos Oliva <olivam@protonmail.com>
2023-07-31 11:43:47 -0700

    Simplifiqué el código de Individual

--- Individual.py
+++ Individual.py
@@ -1,75 +1,28 @@
-from typing import Dict, Optional
+from dataclasses import dataclass, field, asdict
 import evodesign.Sequence as Sequence
-import time
 
 
 
 
 
+@dataclass(order=True)
 class Individual:
   """
   La representación de un individuo (i.e., una secuencia de aminoácidos) en la 
   población del algoritmo evolutivo.
   """
 
-  _next_id = 0
+  sequence: str = field(compare=False)
+  fitness: float = field(default=None)
+  metrics: dict = field(default_factory=dict, compare=False)
 
 
 
   @classmethod
   def random(cls, sequenceLength: int):
     return cls(Sequence.create_random_sequence(sequenceLength))
-
-
-
-  def __init__(self, 
-               sequence: str,
-               fitness: Optional[float] = None,
-               metrics: Optional[Dict[str, float]] = None) -> None:
-    """
-    Constructor.
-    - `sequence`: la secuencia de aminoácidos que representa este individuo.
-    - `id`: el nombre con el que se identificará este individuo en el algoritmo 
-      evolutivo.
-    - `fitness`: el valor de aptitud para este individuo.
-    - `metrics`: un diccionario que contiene valores de diferentes métricas de 
-      calidad para este individuo; estos valores se utilizan para calcular la 
-      aptitud del individuo.
-    """
-    if metrics is None: metrics = {}
-    self.sequence = sequence
-    self.fitness = fitness
-    self.metrics = metrics
   
 
 
   def get_memento(self) -> dict:
-    memento = {
-      'fitness': self.fitness,
-      'metrics': {},
-      'sequence': self.sequence
-    }
-    for key, value in self.metrics.items():
-      memento['metrics'][key] = value
-    return memento
-    
-
-  
-  def __eq__(self, other) -> bool:
-    if other.__class__ != self.__class__:
-      return NotImplemented
-    return self.fitness == other.fitness
-  
-
-
-  def __lt__(self, other):
-    if other.__class__ != self.__class__:
-      return NotImplemented
-    return self.fitness < other.fitness
-  
-
-
-  def __le__(self, other):
-    if other.__class__ != self.__class__:
-      return NotImplemented
-    return (self.fitness < other.fitness) or (self.fitness ==  other.fitness)
+    return asdict(self)
