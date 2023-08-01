0a8e6b246d56fdf921c13371ee14e5d3aedf5095
Carlos Oliva <olivam@protonmail.com>
2023-08-01 13:24:48 -0700

    Removí los comentarios de Sequence

--- Sequence.py
+++ Sequence.py
@@ -1,21 +1,12 @@
-"""Colección de funciones auxiliares para trabajar con secuencias de 
-aminoácidos representadas como cadenas de texto donde cada letra corresponde a 
-una cadena lateral particular.
-"""
 import random
 
 
 
 
 
-"""Las letras válidas para representar los veinte aminoácidos esenciales.
-"""
 AMINOACIDS = 'ACDEFGHIKLMNPQRSTVWY'
 
 
 
 def create_random_sequence(length: int) -> str:
-  """Retorna una una cadena de aminoácidos aleatoria de longitud especificada 
-  por `length`.
-  """
   return ''.join(random.choices(AMINOACIDS, k=length))
