6a4433c04f1e8565611803aea67459a57f920536
Carlos Oliva <olivam@protonmail.com>
2023-08-06 14:53:22 -0700

    Renombré create_random_sequence por simplemente random_sequence

--- Sequence.py
+++ Sequence.py
@@ -8,7 +8,7 @@ AMINOACIDS = 'ACDEFGHIKLMNPQRSTVWY'
 
 
 
-def create_random_sequence(length: int) -> str:
+def random_sequence(length: int) -> str:
   return ''.join(random.choices(AMINOACIDS, k=length))
 
 
