bf37fcd73d6834cd29c3a667c474516bc330e021
Carlos Oliva <olivam@protonmail.com>
2023-09-02 09:31:13 -0700

    Removí una importación incorrecta en Prediction.__init__

--- Prediction/__init__.py
+++ Prediction/__init__.py
@@ -1,5 +1,4 @@
 from .Predictor import Predictor
-from .RemoteApiTest import RemoteApiTest as Predictor_RemoteApiTest
 from .Null import Null as Predictor_Null
 from .ESMFoldRemoteApi import ESMFoldRemoteApi as Predictor_ESMFold_RemoteApi
 from .ESMFoldColab import ESMFoldColab as Predictor_ESMFold_Colab
