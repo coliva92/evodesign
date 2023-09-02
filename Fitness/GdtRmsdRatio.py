03abca1082a4b45fca7c0e9a8650fc979ab008a8
Carlos Oliva <olivam@protonmail.com>
2023-09-02 09:26:37 -0700

    Combiné el Rmsd y el Gdt para crear una nueva función de aptitud: GdtRmsdRatio

--- Fitness/GdtRmsdRatio.py
+++ Fitness/GdtRmsdRatio.py
@@ -6,27 +6,27 @@ from ..Metrics import Rmsd, Gdt as GdtMetric
 
 
 
-class Gdt(FitnessFunction):
+class GdtRmsdRatio(FitnessFunction):
   
   @classmethod
   def name(cls) -> str:
-    return 'Fitness_GDT'
+    return 'Fitness_GdtRmsdRatio'
   
 
 
   @classmethod
   def upper_bound(cls) -> float:
-    return 0.95
+    return 95
   
 
-  
+
   def __init__(self,
                cutoffs: List[float] = [ 1.0, 2.0, 4.0, 8.0 ],
-               alphaCarbonOnly: bool = True
+               carbonAlphaOnly: bool = False
                ) -> None:
     metrics = {
       'rmsd': Rmsd(),
-      'gdt_ts': GdtMetric(cutoffs, alphaCarbonOnly)
+      'gdt': GdtMetric(cutoffs, carbonAlphaOnly)
     }
     super().__init__(metrics)
   
@@ -34,11 +34,12 @@ class Gdt(FitnessFunction):
 
   def params_json(self) -> dict:
     return {
-      'cutoffs': self._metric_calculators['gdt_ts']._cutoffs,
-      'alphaCarbonOnly': self._metric_calculators['gdt_ts']._alpha_carbon_only
+      'cutoffs': self._metric_calculators['gdt']._cutoffs,
+      'alphaCarbonOnly': self._metric_calculators['gdt']._carbon_alpha_only
     }
   
 
 
   def compute_fitness(self, metrics: Dict[str, float]) -> float:
-    return metrics['gdt_ts']
+    return 2 * metrics['gdt'] / metrics['rmsd']
+  
--- Fitness/__init__.py
+++ Fitness/__init__.py
@@ -1,4 +1,2 @@
 from .FitnessFunction import FitnessFunction
-from .Test import Test as Fitness_Test
-from .NegativeRmsd import NegativeRmsd as Fitness_NegativeRMSD
-from .Gdt import Gdt as Fitness_GDT
+from .GdtRmsdRatio import GdtRmsdRatio as Fitness_GdtRmsdRatio
