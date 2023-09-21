586310e9945eace292afe1f33b2e8cb65b7558bd
Carlos Oliva <olivam@protonmail.com>
2023-09-21 14:15:46 -0700

    Implementé una mutación con Blosum

---
+++ GA/Mutation/BlosumSwitches.py
@@ -0,0 +1,51 @@
+from .Mutation import Mutation
+import random
+import evodesign.Sequence as Sequence
+import blosum as bl
+
+
+
+
+
+class BlosumSwitches(Mutation):
+
+  _matrix = bl.BLOSUM(62)
+  _shifts = None
+
+
+
+  @classmethod
+  def name(cls) -> str:
+    return 'GA_Mutation_BlosumSwitches'
+  
+  
+
+  def __init__(self, 
+               probability: float = 1.0, 
+               numSwitches: int = 1
+               ) -> None:
+    super().__init__(probability)
+    self._num_switches = numSwitches
+    if not __class__._shifts:
+      __class__._shifts = {}
+      for residue in Sequence.AMINOACIDS:
+        positives = filter(lambda x: x[1] > 0, 
+                           [ (k, v) for k, v in __class__._matrix[residue] ])
+        __class__._shifts[residue] = [ x[0] for x in positives ]
+  
+
+
+  def params_json(self) -> dict:
+    params = super().params_json()
+    params['numSwitches'] = self._num_switches
+    return params
+
+
+
+  def mutate_sequence(self, sequence: str) -> str:
+    positions = random.sample(range(len(sequence)), self._num_switches)
+    # en Python, las cadenas son inmutables; hay que convertirlas a una lista
+    seq_list = list(sequence)
+    for i in positions:
+      seq_list[i] = random.choice(__class__._shifts[seq_list[i]])
+    return ''.join(seq_list)
--- GA/Mutation/__init__.py
+++ GA/Mutation/__init__.py
@@ -2,3 +2,4 @@ from .Mutation import Mutation
 from .SingleSwitch import SingleSwitch as GA_Mutation_SingleSwitch
 from .MultipleSwitches import MultipleSwitches as GA_Mutation_MultipleSwitches
 from .RandomResetting import RandomResetting as GA_Mutation_RandomResetting
+from .BlosumSwitches import BlosumSwitches as GA_Mutation_BlosumSwitches
