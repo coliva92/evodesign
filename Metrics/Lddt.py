dfcdec8e6aa3026603ab80b4118becca6064865d
Carlos Oliva <olivam@protonmail.com>
2023-09-22 13:59:31 -0700

    Modifiqué cómo se calcula la métrica Lddt

--- Metrics/Lddt.py
+++ Metrics/Lddt.py
@@ -1,6 +1,6 @@
 from Bio.PDB.Atom import Atom
 from .Metric import Metric
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import statistics
 
 
@@ -23,19 +23,29 @@ class Lddt(Metric):
                modelBackbone: List[Atom], 
                referenceBackbone: List[Atom], 
                _: Optional[str] = None) -> float:
-    reference_count = len(list(filter(lambda d: d > 0 and d <= self._radius, 
-                                      [
-                                        a - b 
-                                        for a in referenceBackbone 
-                                        for b in referenceBackbone 
-                                      ])))
-    model_distances = list(filter(lambda d: d > 0 and d <= self._radius,
-                                  [ 
-                                    a - b 
-                                    for a in modelBackbone 
-                                    for b in modelBackbone 
-                                  ]))
-    return statistics.fmean([ 
-      len(list(filter(lambda d: d <= c, model_distances))) / reference_count
-      for c in self._cutoffs 
+    
+    def preserved(i: int, j: int, cutoff: float) -> bool:
+      a = modelBackbone[i]
+      b = modelBackbone[j]
+      x = referenceBackbone[i]
+      y = referenceBackbone[j]
+      return abs((x - y) - (a - b)) <= cutoff
+    
+    reference_set = {
+      { i, j }
+      for i, a in enumerate(referenceBackbone)
+      for j, b in enumerate(referenceBackbone)
+      if a - b > 0 and a - b <= self._radius
+    }
+    model_distances = [
+        ( i, j )
+        for i, a in enumerate(modelBackbone)
+        for j, b in enumerate(modelBackbone)
+        if a - b > 0 and a - b <= self._radius and { i, j } in reference_set
+    ]
+    
+    n = len(reference_set)
+    return statistics.fmean([
+      sum([ preserved(x[0], x[1], c) for x in model_distances ]) / n
+      for c in self._cutoffs
     ])
