df2f8073c093b6aeee81ceda142e80a64f19b810
Carlos Oliva <olivam@protonmail.com>
2023-10-04 21:26:31 -0700

    Corregí la exportación de Fitness.Lddt

--- Fitness/__init__.py
+++ Fitness/__init__.py
@@ -2,3 +2,4 @@ from .FitnessFunction import FitnessFunction
 from .Gdt import Gdt as Fitness_Gdt
 from .RmsdGdtLddt import RmsdGdtLddt as Fitness_RmsdGdtLddt
 from .RmsdGdtEnergyScore import RmsdGdtEnergyScore as Fitness_RmsdGdtEnergyScore
+from .Lddt import Lddt as Fitness_Lddt
