bfa6bbff6d7e7d0ac674d2fbc0612ae1ee43a7b0
Carlos Oliva <olivam@protonmail.com>
2023-10-05 17:57:56 -0700

    Corregí la exportación de Fitness.NegativeRastrigin

--- Fitness/__init__.py
+++ Fitness/__init__.py
@@ -3,3 +3,4 @@ from .Gdt import Gdt as Fitness_Gdt
 from .RmsdGdtLddt import RmsdGdtLddt as Fitness_RmsdGdtLddt
 from .RmsdGdtEnergyScore import RmsdGdtEnergyScore as Fitness_RmsdGdtEnergyScore
 from .Lddt import Lddt as Fitness_Lddt
+from .NegativeRastrigin import NegativeRastrigin as Fitness_NegativeRastrigin
