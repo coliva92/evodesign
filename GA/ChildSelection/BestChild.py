3b7e83e0d8dd6b73a7d0668681240dd72a15e0eb
Carlos Oliva <olivam@protonmail.com>
2023-10-12 16:35:58 -0700

    GA_ChildrenSelection_SingleBest ahora tiene un factor de aleatoriedad

--- GA/ChildSelection/BestChild.py
+++ GA/ChildSelection/BestChild.py
@@ -1,5 +1,6 @@
 from .ChildSelection import ChildSelection
 from evodesign import Population
+import evodesign.Choice as Choice
 
 
 
@@ -12,11 +13,28 @@ class BestChild(ChildSelection):
     return 'GA_ChildrenSelection_SingleBest'
   
 
+
+  def __init__(self, maxInputSize: int, probability: float = 1.0) -> None:
+    super().__init__(maxInputSize)
+    self._probability = probability
+    self._weights = ( probability, 1.0 - probability )
+  
+
+
+  def params_json(self) -> dict:
+    params = super().params_json()
+    params['probability'] = self._probability
+    return params
+  
+
   
   def select_children(self, children: Population) -> Population:
     # suponemos que la recombinación produjo dos hijos por cada par de padres
-    children.individuals = [
-      sister if sister > brother else brother
-      for sister, brother in zip(children[0::2], children[1::2])
-    ]
+    temp_children = []
+    for sister, brother in zip(children[0::2], children[1::2]):
+      if Choice.flip_coin(self._weights):
+        temp_children.append(sister if sister > brother else brother)
+      else:
+        temp_children.append(sister if Choice.flip_coin() else brother)
+    children.individuals = temp_children
     return children
