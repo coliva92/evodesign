9e37adcbaa9a37cd47770aa4b0ccec97ad9c54ab
Carlos Oliva <olivam@protonmail.com>
2023-12-21 23:09:42 -0800

    Ya se puede importar el predictor de AlphaFold

--- Prediction/__init__.py
+++ Prediction/__init__.py
@@ -2,3 +2,4 @@ from .Predictor import Predictor
 from .Null import Null as Predictor_None
 from .ESMFoldRemoteApi import ESMFoldRemoteApi as Predictor_ESMFold_RemoteApi
 from .ESMFoldColab import ESMFoldColab as Predictor_ESMFold_Colab
+from .AlphaFold import AlphaFold as Predictor_AlphaFold
