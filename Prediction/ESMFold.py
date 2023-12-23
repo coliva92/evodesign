3f995f2995a0885b7687771371d0439f9f175a9e
Carlos Oliva <olivam@protonmail.com>
2023-12-22 19:33:53 -0800

    Agregué ESMFold como un predictor

---
+++ Prediction/ESMFold.py
@@ -0,0 +1,36 @@
+from .Predictor import Predictor
+
+
+
+
+
+class ESMFold(Predictor):
+
+  _model = None
+  
+
+
+  @classmethod
+  def name(cls) -> str:
+    return 'Predictor_ESMFold'
+  
+
+
+  def predict_structure(self, 
+                        sequence: str, 
+                        pdbFilename: str
+                        ) -> None:
+    import esm
+    if not ESMFold._model:
+      ESMFold._model = esm.pretrained.esmfold_v1()
+      ESMFold._model.eval().cuda().requires_grad_(False)
+      ESMFold._model.set_chunk_size(128)
+    prediction = ESMFold._model.infer_pdb(sequence)
+    with open(pdbFilename, 'wt', encoding='utf-8') as pdb_file:
+      pdb_file.write(prediction)
+
+
+
+  def __del__(self) -> None:
+    import torch
+    torch.cuda.empty_cache()
--- Prediction/__init__.py
+++ Prediction/__init__.py
@@ -3,3 +3,4 @@ from .Null import Null as Predictor_None
 from .ESMFoldRemoteApi import ESMFoldRemoteApi as Predictor_ESMFold_RemoteApi
 from .ESMFoldColab import ESMFoldColab as Predictor_ESMFold_Colab
 from .AlphaFold import AlphaFold as Predictor_AlphaFold
+from .ESMFold import ESMFold as Predictor_ESMFold
