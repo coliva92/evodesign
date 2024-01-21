7cb9428c830cf5ad7011f99d46cef8f8b8a1c787
Carlos Oliva <olivam@protonmail.com>
2024-01-21 15:52:26 -0800

    Agregué las pruebas unitarias de Sequence

---
+++ tests/sequence_functions.py
@@ -0,0 +1,17 @@
+import evodesign.Sequence as Sequence
+from evodesign import Random
+
+
+
+sequence = Sequence.random_sequence(10)
+test1_passed = type(sequence) == str and \
+               len(sequence) and \
+               sum([ i in Sequence.AMINO_ACIDS for i in sequence ]) == len(sequence)
+
+rng = Random.get_rng()
+old_letter = rng.choice(len(Sequence.AMINO_ACIDS))
+new_letter = Sequence.switch_residue(old_letter)
+test2_passed = type(new_letter) == str and \
+               len(new_letter) == 1 and \
+               new_letter != old_letter
+print('PASSED' if test1_passed and test2_passed else 'FAILED')
