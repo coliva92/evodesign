2d15591097044d454fe6cf10a4413f396d943e6f
Carlos Oliva <olivam@protonmail.com>
2024-02-17 10:11:39 -0800

    Implementé una herramienta para duplicar workspaces con la intención de reproducir experimentos

---
+++ evodesign_utils/reproduce_workspace.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from evodesign.Population import Population
+import pandas as pd
+import shutil
+import os
+import glob
+
+
+
+
+
+parser = ArgumentParser(prog='reproduce',
+                        description='A utility script for replicating an '
+                                    'EvoDesign workspace in order to reproduce '
+                                    'the corresponding experiment')
+parser.add_argument('source_workspace',
+                    help='path to the folder that contains the original '
+                         'workspace to be reproduced')
+parser.add_argument('destination_workspace',
+                    help='path to the folder where the results from the '
+                         'reproduced experiment will be stored')
+args = parser.parse_args()
+
+pdbs_dir = f'{args.destination_workspace}/pdbs'
+os.makedirs(pdbs_dir)
+for filepath in glob.glob(f'{args.source_workspace}/pdbs/prot_0001_*.pdb'):
+  shutil.copy(filepath, pdbs_dir)
+
+populations_dir = f'{args.destination_workspace}/populations'
+os.makedirs(populations_dir)
+shutil.copy(f'{args.source_workspace}/populations/pop_0001.csv', 
+            populations_dir)
+# remove fitness columns
+pop = Population.create_random(1, 4)
+df = pd.read_csv(f'{populations_dir}/pop_0001.csv')
+df = df[pop.columns]
+df.to_csv(f'{populations_dir}/pop_0001.csv')
+
+shutil.copy(f'{args.source_workspace}/initial_rng_state.json',
+            args.destination_workspace)
