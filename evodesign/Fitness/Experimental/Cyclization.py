96e86b6dff50983334e92166e4beb4d83614a5ee
Carlos Oliva <olivam@protonmail.com>
2024-02-17 18:13:10 -0800

    Cambié el cálculo de Fitness.Experimental.Cyclization y eliminé Fitness.Experimental.Cyclization2 por ya no ser necesario

--- evodesign/Fitness/Experimental/Cyclization.py
+++ evodesign/Fitness/Experimental/Cyclization.py
@@ -1,5 +1,4 @@
 from ..FitnessFunction import FitnessFunction
-from typing import List
 from ...Metrics.Experimental.Cyclization import Cyclization as CycMetric
 from ...Metrics.Rmsd import Rmsd
 import numpy as np
@@ -26,7 +25,8 @@ class Cyclization(FitnessFunction):
     params = super()._params()
     params['rmsdBound'] = self._rmsd_bound
     params['cyclizationBound'] = self._cyc_bound
-    params['weights'] = self._weights
+    params['rmsdWeight'] = self._rmsd_weight
+    params['cycWeight'] = self._cyc_weight
     return params
   
 
@@ -35,11 +35,15 @@ class Cyclization(FitnessFunction):
                upperBound: float = 1.0,
                rmsdBound: float = 2.0,
                cyclizationBound: float = 1.32,
-               weights: List[float] = [ 1.0, 0.5 ]) -> None:
+               rmsdWeight: float = 1.0,
+               cycWeight: float = 1.0
+               ) -> None:
     super().__init__(upperBound, [ CycMetric(), Rmsd() ])
     self._rmsd_bound = rmsdBound
     self._cyc_bound = cyclizationBound
-    self._weights = weights
+    self._rmsd_weight = rmsdWeight
+    self._cyc_weight = cycWeight
+    self._weights = np.array([ rmsdWeight, cycWeight ])
 
 
 
@@ -48,6 +52,7 @@ class Cyclization(FitnessFunction):
     c = kwargs['cyclization']
     r_min = self._rmsd_bound
     c_min = self._cyc_bound
-    return np.average(np.array([ r_min / (1.0 + r), c_min / (1.0 + c) ]), 
-                      weights=self._weights)
+    a = 1.0 / (1.0 + (r - r_min))
+    b = 1.0 / (1.0 + (c - c_min))
+    return np.average(np.array([ a, b ]), weights=self._weights)
   
