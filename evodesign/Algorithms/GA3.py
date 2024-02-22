ccc6574b670f84e81edec377ccfa53d4cb8c0220
Carlos Oliva <olivam@protonmail.com>
2024-02-22 01:14:42 -0800

    Implementé Algorithms.GA con sus pruebas correspondientes

---
+++ evodesign/Algorithms/GA3.py
@@ -0,0 +1,53 @@
+from typing import List
+from Bio.PDB.Atom import Atom
+from .GenericGA import GenericGA
+from ..Statistics import Statistics
+import evodesign.Operators as op
+import pandas as pd
+
+
+
+
+
+class GA3(GenericGA):
+
+  @classmethod
+  def _class_name(cls) -> str:
+    return 'Algorithms.GA3'
+  
+
+
+  def compute_statistics(self, population: pd.DataFrame) -> pd.Series:
+    top_solution = population.iloc[0].copy()
+    survivors = population[population['survivor']]
+    top_solution['sequence_identity'] = \
+      Statistics.average_sequence_identity(survivors)
+    top_solution['lost_amino_acids'] = \
+      Statistics.average_amino_acid_loss(survivors)
+    return top_solution
+  
+
+
+  def replacement(self, 
+                  population: pd.DataFrame, 
+                  children: pd.DataFrame, 
+                  reference: List[Atom]
+                  ) -> pd.DataFrame:
+    elite = population.iloc[0].copy()
+    elite['survivor'] = False
+    mixed = pd.concat([ elite.to_frame().T, children ], ignore_index=True)
+    for i, row in mixed.iloc[1:].iterrows():
+      if op.comes_first(elite, row, self._sort_cols, self._sort_ascending):
+        continue
+      # put winning row in the elite position
+      mixed.iloc[0] = row
+      # move last row to the winning row's original position
+      mixed.iloc[i] = mixed.iloc[-1]
+      # put elite solution (which is no longer the elite) in the last position
+      mixed.iloc[-1] = elite
+      break
+    # if last pop's elite solution wins, it is preserved;
+    # if it loses to another solution in children, it is removed and all
+    # children survive to the next generation
+    mixed.loc[:self._pop_size - 1, 'survivor'] = True
+    return mixed
---
+++ tests/TestGA3.py
@@ -0,0 +1,104 @@
+from unittest import TestCase
+from evodesign.Algorithms.GA3 import GA3
+from evodesign.GA.Selection.Tournament import Tournament
+from evodesign.GA.Recombination.UniformCrossover import UniformCrossover
+from evodesign.GA.Mutation.RandomResetting import RandomResetting
+from evodesign.Fitness.Rastrigin import Rastrigin
+from evodesign.Prediction.Null import Null
+import pandas as pd
+
+
+
+
+
+class GA3Tests(TestCase):
+
+  def setUp(self):
+    self.algo = GA3(maxGenerations=100,
+                    popSize=5,
+                    predictor=Null(),
+                    fitnessFn=Rastrigin(),
+                    selection=Tournament(tournamentSize=2,
+                                         sortColumns=[ 'fitness_rastrigin' ],
+                                         sortAscending=[ False ]),
+                    recombination=UniformCrossover(),
+                    mutation=RandomResetting(),
+                    sortColumns=[ 'fitness_rastrigin' ],
+                    sortAscending=[ False ])
+    self.pop = pd.DataFrame({
+      'sequence_id': [ 'A', 'B', 'C', 'D', 'E', 'X', 'Z' ],
+      'fitness_rastrigin': [ -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7 ],
+      'survivor': [ True, True, True, True, True, False, False ]
+    })
+    self.children = pd.DataFrame({
+      'sequence_id': [ 'F', 'G', 'H', 'I', 'J' ],
+      'fitness_rastrigin': [ -0.81, -0.82, -0.83, -0.84, -0.85 ],
+      'survivor': [ False, False, False, False, False ]
+    })
+    self.correct_next_pop = pd.DataFrame({
+      'sequence_id': [ 'A', 'F', 'G', 'H', 'I', 'J' ],
+      'fitness_rastrigin': [ -0.1, -0.81, -0.82, -0.83, -0.84, -0.85 ],
+      'survivor': [ True, True, True, True, True, False ]
+    })
+    self.children_winner_first = pd.DataFrame({
+      'sequence_id': [ 'F', 'G', 'H', 'I', 'J' ],
+      'fitness_rastrigin': [ -0.05, -0.82, -0.83, -0.84, -0.85 ],
+      'survivor': [ False, False, False, False, False ]
+    })
+    self.correct_next_pop_winner_first = pd.DataFrame({
+      'sequence_id': [ 'F', 'J', 'G', 'H', 'I', 'A' ],
+      'fitness_rastrigin': [ -0.05, -0.85, -0.82, -0.83, -0.84, -0.1 ],
+      'survivor': [ True, True, True, True, True, False ]
+    })
+    self.children_winner_mid = pd.DataFrame({
+      'sequence_id': [ 'F', 'G', 'H', 'I', 'J' ],
+      'fitness_rastrigin': [ -0.81, -0.82, -0.05, -0.84, -0.85 ],
+      'survivor': [ False, False, False, False, False ]
+    })
+    self.correct_next_pop_winner_mid = pd.DataFrame({
+      'sequence_id': [ 'H', 'F', 'G', 'J', 'I', 'A' ],
+      'fitness_rastrigin': [ -0.05, -0.81, -0.82, -0.85, -0.84, -0.1 ],
+      'survivor': [ True, True, True, True, True, False ]
+    })
+    self.children_winner_last = pd.DataFrame({
+      'sequence_id': [ 'F', 'G', 'H', 'I', 'J' ],
+      'fitness_rastrigin': [ -0.81, -0.82, -0.83, -0.84, -0.05 ],
+      'survivor': [ False, False, False, False, False ]
+    })
+    self.correct_next_pop_winner_last = pd.DataFrame({
+      'sequence_id': [ 'J', 'F', 'G', 'H', 'I', 'A' ],
+      'fitness_rastrigin': [ -0.05, -0.81, -0.82, -0.83, -0.84, -0.1 ],
+      'survivor': [ True, True, True, True, True, False ]
+    })
+  
+
+
+  def test_replacement(self):
+    next_pop = self.algo.replacement(self.pop, self.children, [])
+    comparison = next_pop == self.correct_next_pop
+    rows_equal = comparison.all(axis=1)
+    self.assertEqual(rows_equal.all(), True)
+  
+
+
+  def test_replacement_winner_first(self):
+    next_pop = self.algo.replacement(self.pop, self.children_winner_first, [])
+    comparison = next_pop == self.correct_next_pop_winner_first
+    rows_equal = comparison.all(axis=1)
+    self.assertEqual(rows_equal.all(), True)
+
+
+
+  def test_replacement_winner_mid(self):
+    next_pop = self.algo.replacement(self.pop, self.children_winner_mid, [])
+    comparison = next_pop == self.correct_next_pop_winner_mid
+    rows_equal = comparison.all(axis=1)
+    self.assertEqual(rows_equal.all(), True)
+  
+
+
+  def test_replacement_winner_last(self):
+    next_pop = self.algo.replacement(self.pop, self.children_winner_last, [])
+    comparison = next_pop == self.correct_next_pop_winner_last
+    rows_equal = comparison.all(axis=1)
+    self.assertEqual(rows_equal.all(), True)
