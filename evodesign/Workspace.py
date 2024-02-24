a99e788099de118eb9b94b1ef571a75f7c33c69f
Carlos Oliva <olivam@protonmail.com>
2024-02-24 10:30:55 -0800

    Actualicé el hash del último commit que se guarda en el workspace

--- evodesign/Workspace.py
+++ evodesign/Workspace.py
@@ -66,7 +66,7 @@ class Workspace:
     os.makedirs(self.root_dir, exist_ok=True)
     filepath = f'{self.root_dir}/commit_hash.txt'
     with open(filepath, 'wt', encoding='utf-8') as txt_file:
-      txt_file.write('bfee6af\n')
+      txt_file.write('df257c1\n')
 
 
 
