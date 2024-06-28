1201517b46b3366d2d338eea37090b6b4a8863ca
Carlos Oliva <olivam@protonmail.com>
2024-06-28 09:17:09 -0300

    Cambié el alias con el que se importan algunas clases en Settings para que corresponda con su nombre calificado

--- evodesign/Settings.py
+++ evodesign/Settings.py
@@ -10,7 +10,7 @@ from .Metrics.ContactMapRms import ContactMapRms as Metrics_ContactMapRms
 from .Metrics.Cyclization import Cyclization as Metrics_Cyclization
 from .Metrics.TMScore import TMScore as Metrics_TMScore
 from .Metrics.PyRosettaRef2015 import PyRosettaRef2015 \
-  as Metrics_Ref2015
+  as Metrics_PyRosettaRef2015
 from .Metrics.iLearnDescriptors import iLearnDescriptors \
   as Metrics_iLearnDescriptors
 from .Metrics.ESM2Descriptors import ESM2Descriptors as Metrics_ESM2Descriptors
@@ -41,11 +41,11 @@ from .GA.Selection.Overselection import Overselection \
 from .GA.Selection.Tournament import Tournament as GA_Selection_Tournament
 from .GA.Selection.Uniform import Uniform as GA_Selection_Uniform
 from .Prediction.AlphaFold import AlphaFold as Prediction_AlphaFold
-from .Prediction.ESMFold import ESMFold as Prediction_ESMFold_Local
-from .Prediction.ESMFoldColab import ESMFoldColab as Prediction_ESMFold_Colab
+from .Prediction.ESMFold import ESMFold as Prediction_ESMFold
+from .Prediction.ESMFoldColab import ESMFoldColab as Prediction_ESMFoldColab
 from .Prediction.ESMFoldRemoteApi import ESMFoldRemoteApi \
-  as Prediction_ESMFold_RemoteApi
-from .Prediction.Null import Null as Prediction_None
+  as Prediction_ESMFoldRemoteApi
+from .Prediction.Null import Null as Prediction_Null
 import copy
 
 
