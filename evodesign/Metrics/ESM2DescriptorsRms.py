8e69a102fdc12f9e85ebc30c1c57b113fd65482e
Carlos Oliva <olivam@protonmail.com>
2024-07-02 00:19:30 -0300

    Modifiqué Metrics.ESM2DescriptorsRms para que reciba como entrada de constructor una instancia explícita de Metrics.ESM2Descriptors

--- evodesign/Metrics/ESM2DescriptorsRms.py
+++ evodesign/Metrics/ESM2DescriptorsRms.py
@@ -15,14 +15,14 @@ class ESM2DescriptorsRms(Metric):
 
 
   def _params(self) -> dict:
-    return self._esm2_desc._params()
+    return { 'descriptor_calc': self._descriptor_calc.settings() }
   
 
 
-  def __init__(self, use_gpu: bool = True) -> None:
+  def __init__(self, descriptor_calc: ESM2Descriptors) -> None:
     super().__init__()
     self._ref_vector = None
-    self._esm2_desc = ESM2Descriptors(use_gpu)
+    self._descriptor_calc = descriptor_calc 
   
 
 
@@ -32,9 +32,9 @@ class ESM2DescriptorsRms(Metric):
       workspace = Workspace.instance()
       ref_csv_path = f'{workspace.esm2_dir}/reference.csv'
       self._ref_vector = \
-        self._esm2_desc.compute_descriptor_vector(ref_seq, ref_csv_path)
+        self._descriptor_calc.compute_descriptor_vector(ref_seq, ref_csv_path)
     other_metrics = kwargs['otherMetrics']
-    self._esm2_desc(**kwargs)
+    self._descriptor_calc(**kwargs)
     csv_path = other_metrics['esm2_descriptor']
-    vector = self._esm2_desc.load_vector_from_csv(csv_path)
+    vector = self._descriptor_calc.load_vector_from_csv(csv_path)
     return np.sqrt(np.mean((self._ref_vector - vector) ** 2))
