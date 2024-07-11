3decf4c0df8c9c22927c13d55d416bc10425aaf8
Carlos Oliva <olivam@protonmail.com>
2024-07-11 01:49:50 -0300

    Modifiqué la identación de Algorithms.GASimpleElitism y Algorithms.GASteadyState

--- evodesign/Algorithms/GASteadyState.py
+++ evodesign/Algorithms/GASteadyState.py
@@ -8,62 +8,62 @@ import pandas as pd
 
 
 class GASteadyState(GenericGA):
-  
-  def compute_statistics(self, population: pd.DataFrame) -> pd.Series:
-    """
-    Computes the population statistics.
 
-    Parameters
-    ----------
-    population : pd.DataFrame
-        The population which statistics will be computed
+    def compute_statistics(self, population: pd.DataFrame) -> pd.Series:
+        """
+        Computes the population statistics.
 
-    Returns
-    -------
-    pd.Series
-        The top individual in the population, along the computed statistics.
-    """
-    top_solution = population.iloc[0].copy()
-    survivors = population[population['survivor']]
-    top_solution['sequence_identity'] = \
-      Statistics.average_sequence_identity(survivors)
-    top_solution['lost_amino_acids'] = \
-      Statistics.average_amino_acid_loss(survivors)
-    return top_solution
-  
+        Parameters
+        ----------
+        population : pd.DataFrame
+            The population which statistics will be computed
+
+        Returns
+        -------
+        pd.Series
+            The top individual in the population, along the computed statistics.
+        """
+        top_solution = population.iloc[0].copy()
+        survivors = population[population['survivor']]
+        top_solution['sequence_identity'] = \
+            Statistics.average_sequence_identity(survivors)
+        top_solution['lost_amino_acids'] = \
+            Statistics.average_amino_acid_loss(survivors)
+        return top_solution
 
 
-  def replacement(self, 
-                  population: pd.DataFrame, 
-                  children: pd.DataFrame
-                  ) -> pd.DataFrame:
-    """
-    Creates a new population by selecting which individuals from last 
-    generation's population and current generation's children should survive
-    to the next generation.
 
-    Parameters
-    ----------
-    population : pd.DataFrame
-        The population from last generation.
-    children : pd.DataFrame
-        The children created at the current generation.
+    def replacement(self,
+                    population: pd.DataFrame,
+                    children: pd.DataFrame
+                    ) -> pd.DataFrame:
+        """
+        Creates a new population by selecting which individuals from last 
+        generation's population and current generation's children should survive
+        to the next generation.
 
-    Returns
-    -------
-    pd.DataFrame
-        The population for next generation, composed of the selected survivors.
-    """
-    children.sort_values(by=self._sort_columns, 
-                         ascending=self._sort_ascending, 
-                         inplace=True,
-                         ignore_index=True)
-    last_pop = population[population['survivor']].copy()
-    last_pop['survivor'] = False
-    last_pop['generation_id'] = children.iloc[0]['generation_id']
-    mixed = Utils.merge(last_pop, 
-                        children, 
-                        self._sort_columns, 
-                        self._sort_ascending)
-    mixed.loc[:self._pop_size - 1, 'survivor'] = True
-    return mixed
+        Parameters
+        ----------
+        population : pd.DataFrame
+            The population from last generation.
+        children : pd.DataFrame
+            The children created at the current generation.
+
+        Returns
+        -------
+        pd.DataFrame
+            The population for next generation, composed of the selected survivors.
+        """
+        children.sort_values(by=self._sort_columns,
+                             ascending=self._sort_ascending,
+                             inplace=True,
+                             ignore_index=True)
+        last_pop = population[population['survivor']].copy()
+        last_pop['survivor'] = False
+        last_pop['generation_id'] = children.iloc[0]['generation_id']
+        mixed = Utils.merge(last_pop,
+                            children,
+                            self._sort_columns,
+                            self._sort_ascending)
+        mixed.loc[:self._pop_size - 1, 'survivor'] = True
+        return mixed
