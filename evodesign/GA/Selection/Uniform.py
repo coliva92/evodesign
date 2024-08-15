147d592f9153ebe560cd72560e7d135765ee1bf1
Carlos Oliva <olivam@protonmail.com>
2024-08-15 11:49:50 -0300

    El constructor de GA.Selection.Uniform ahora acepta un parámetro para hacer la selección con o sin reemplazo.

--- evodesign/GA/Selection/Uniform.py
+++ evodesign/GA/Selection/Uniform.py
@@ -8,26 +8,49 @@ import pandas as pd
 
 class Uniform(Selection):
   
-  def select_parents(self, 
-                     population: pd.DataFrame,
-                     context: Context
-                     ) -> pd.DataFrame:
-    """
-    Selects a subset of individuals from the given population.
+    def _params(self) -> dict:
+       params = super()._params()
+       params["replacement"] = self._replacement
+       return params
+    
 
-    Parameters
-    ----------
-    population : pandas.DataFrame
-        The population to be sampled.
-    context : Context
-        The context data used by the calling evolutionary algorithm.
 
-    Returns
-    -------
-    pandas.DataFrame
-        The selected subset of individuals.
-    """
-    selection = context.rng.choice(population.index, 
-                                   len(population), 
-                                   replace=False)
-    return population.loc[selection]
+    def __init__(self, replacement: bool = False) -> None:
+        """
+        Selection operation in which a subset of population individuals is choosed
+        randomly with uniform probability. 
+
+        Parameters
+        ----------
+        replace : bool, optional
+            Indicates if the random selection should be performed with or without 
+            replacement. Default is `False`.
+        """
+        super().__init__()
+        self._replacement = replacement
+    
+
+
+    def select_parents(self, 
+                       population: pd.DataFrame,
+                       context: Context
+                       ) -> pd.DataFrame:
+      """
+      Selects a subset of individuals from the given population.
+  
+      Parameters
+      ----------
+      population : pandas.DataFrame
+          The population to be sampled.
+      context : Context
+          The context data used by the calling evolutionary algorithm.
+  
+      Returns
+      -------
+      pandas.DataFrame
+          The selected subset of individuals.
+      """
+      selection = context.rng.choice(population.index, 
+                                     len(population), 
+                                     replace=self._replacement)
+      return population.loc[selection]
