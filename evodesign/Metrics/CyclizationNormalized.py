454cd1b21d558457d7e98ba621c98a0518e68d6d
Carlos Oliva <olivam@protonmail.com>
2024-10-03 14:48:37 -0300

    Sustituí la clase `Metrics.Normalization.Cyclization` por `Metrics.CyclizationNormalized` para aprovechar las nuevas clases creadas

---
+++ evodesign/Metrics/CyclizationNormalized.py
@@ -0,0 +1,44 @@
+from .Metric import Metric
+from .Cyclization import Cyclization as Cyclization
+from .ZScore import ZScore
+from ..Context import Context
+from typing import Optional, List
+from Bio.PDB.Atom import Atom
+import pandas as pd
+import numpy as np
+
+
+
+
+
+class CyclizationNormalized(Metric):
+
+    def __init__(self, 
+                 scaling_factor: float = 1.0,
+                 metric: Optional[ZScore] = None,
+                 column: Optional[str] = None
+                 ) -> None:
+        super().__init__(column)
+        if metric is None:
+            metric = ZScore(mean=1.3248119, 
+                            standard_deviation=0.10498072, 
+                            metric=Cyclization())
+        self._metric = metric
+        self._scaling_factor = scaling_factor
+    
+
+
+    def _compute_values(self, 
+                        backbone: List[Atom],
+                        data: pd.Series,
+                        context: Context
+                        ) -> pd.Series:
+        data = self._metric(backbone, data, context)
+        cyc = self._normalize(data[self._metric.column_name()])
+        data[self.column_name()] = cyc
+        return data
+
+
+
+    def _normalize(self, z: float) -> float:
+        return 1.0 / (1.0 + self._scaling_factor * np.abs(z))
