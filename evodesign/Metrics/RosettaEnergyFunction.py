746c7d5b6e79a4048f74eeab3e2d1a1d80f7a229
Carlos Oliva <olivam@protonmail.com>
2024-10-05 17:31:33 -0300

    Modifiqué Metrics.RosettaEnergyFunction para que la interfaz de Rosetta ya no es una variable de clase.

--- evodesign/Metrics/RosettaEnergyFunction.py
+++ evodesign/Metrics/RosettaEnergyFunction.py
@@ -1,5 +1,5 @@
 from .Metric import Metric
-from typing import Optional, List, Dict
+from typing import Optional, List
 from ..Context import Context
 from Bio.PDB.Atom import Atom
 import pandas as pd
@@ -11,12 +11,9 @@ import pyrosetta
 
 class RosettaEnergyFunction(Metric):
 
-  _score_fn = None
-  
-
-
   def __init__(self, column: Optional[str] = None) -> None:
     super().__init__(column)
+    self._score_fn = None
   
 
 
