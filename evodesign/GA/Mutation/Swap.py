a235dc7c4252fcf34c54775aa477bc9c57089393
Carlos Oliva <olivam@protonmail.com>
2024-10-11 23:38:34 -0300

    Refactoricé GA.Mutation.* para sustituir el uso de Pandas por NumPy

--- evodesign/GA/Mutation/Swap.py
+++ evodesign/GA/Mutation/Swap.py
@@ -1,84 +1,43 @@
 from .Mutation import Mutation
-import evodesign.Sequence as Sequence
+from evodesign.Sequence import AMINO_ACID_INT_VALUES
 import numpy as np
-from typing import Optional, List, Dict
-
-
-
+import numpy.typing as npt
+from typing import Optional
 
 
 class Swap(Mutation):
-  
-  def _params(self) -> dict:
-    params = super()._params()
-    params['num_swaps'] = self._num_swaps
-    return params
-  
 
+    def __init__(self, mutation_probability: float = 1.0, num_swaps: int = 1) -> None:
+        super().__init__(mutation_probability)
+        self.num_swaps = num_swaps
 
-  def __init__(self, 
-               mutation_prob: float = 1.0,
-               num_swaps: int = 1
-               ) -> None:
-    """
-    Mutation operation in which a given number of residues are randomly 
-    chosen from the given amino acid sequence (with uniform probability and
-    without replacement) and exchanged for a different amino acid.
-    The replacing amino acid is chosen randomly with uniform probability. 
-
-    For example, given the sequence 'AAAAAA', and assuming that only one 
-    residue--the third one--is to be exchanged, then this operation would 
-    produce the sequence'AADAAA'.
-
-    Parameters
-    ----------
-    mutation_prob : float, optional
-        The probability for applying this mutation operator over a given
-        sequence. The default is 1.0 (i.e., mutate every and all sequences
-        in a given population).
-    num_swaps : int, optional
-        The number of amino acids that will be exchanged in the sequence.
-        The default is 1.
-    """
-    super().__init__(mutation_prob)
-    self._num_swaps = num_swaps
-  
-
-
-  def mutate_sequence(self, 
-                      sequence: str,
-                      rng: np.random.Generator,
-                      allowed_letters: Optional[Dict[int, List[str]]] = None
-                      ) -> str:
-    """
-    Modifies some amino acids residues in the given sequence.
-
-    Parameters
-    ----------
-    sequence : str
-        The amino acid sequence that will be modified. Each residue in this
-        sequence must be represented by a single letter corresponding to one 
-        of the 20 essential amino acids.
-    rng : numpy.random.Generator
-        The RNG used to mutate the sequence.
-    allowed_letters : Dict[int, List[str]], optional
-        A description of which letters are allowed to be chosen for certain positions
-        in the sequence. If no letter pool is specified for a given position, then no
-        restrictions in the letter selection will be imposed at that position. Default
-        is `None`, which means that any amino acid letter can be chosen at any position.
-
-    Returns
-    -------
-    str
-        The modified sequence.
-    """
-    indices = rng.choice(len(sequence), self._num_swaps, replace=False)
-    # in Python, strings are immutable
-    seq_list = list(sequence)
-    if allowed_letters is None:
-      for i in indices:
-        seq_list[i] = Sequence.swap_letter(rng, seq_list[i])
-      return ''.join(seq_list)
-    for i in indices:
-      seq_list[i] = Sequence.swap_letter(rng, seq_list[i], allowed_letters[i])
-    return ''.join(seq_list)
+    def do(
+        self,
+        rng: np.random.Generator,
+        children: npt.NDArray[np.int64],
+        constraints: Optional[npt.NDArray[np.float64]] = None,
+    ) -> npt.NDArray[np.int64]:
+        rows_mask = rng.random(children.shape[0]) <= self.mutation_probability
+        swap_indices = rng.integers(
+            0, children.shape[1], (children.shape[0], self.num_swaps)
+        ).T
+        if constraints is None:
+            mutations = rng.choice(AMINO_ACID_INT_VALUES[1:], size=children.shape)
+            children[rows_mask, swap_indices[rows_mask]] = (
+                children[rows_mask, swap_indices[rows_mask]]
+                + mutations[rows_mask, swap_indices[rows_mask]]
+            ) % AMINO_ACID_INT_VALUES.shape[0]
+        else:
+            # constraints.shape = sequence_length x len(AMINO_ACID_INT_VALUES)
+            mutations = np.vstack(
+                [
+                    rng.choice(
+                        AMINO_ACID_INT_VALUES, children.shape[0], p=constraints[i]
+                    )
+                    for i in range(constraints.shape[0])
+                ]
+            ).T
+            children[rows_mask, swap_indices[rows_mask]] = mutations[
+                rows_mask, swap_indices[rows_mask]
+            ]
+        return children
