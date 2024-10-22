a552b0989f406815110fadb5d3b789fa90680e3e
Carlos Oliva <olivam@protonmail.com>
2024-10-22 11:43:46 -0300

    Removí la dependencia a Workspace en Prediction.AlphaFold

--- evodesign/Prediction/AlphaFold.py
+++ evodesign/Prediction/AlphaFold.py
@@ -1,9 +1,7 @@
 from .Predictor import Predictor
-from ..Workspace import Workspace
 import subprocess
 import os
 import shutil
-from typing import Optional
 
 
 class AlphaFold(Predictor):
@@ -12,12 +10,12 @@ class AlphaFold(Predictor):
         self,
         path_to_create_fakemsa_py: str,
         path_to_run_alphafold_py: str,
+        output_dir: str,
         mgnify_database_path: str,
         data_dir: str,
         max_template_date: str = "2020-05-14",
         model_preset: str = "monomer",
-        db_preset: str = "reduced_dbs",
-        output_dir: Optional[str] = None,
+        db_preset: str = "reduced_dbs"
     ) -> None:
         super().__init__()
         self.path_to_create_fakemsa_py = path_to_create_fakemsa_py  # https://github.com/Zuricho/ParaFold_dev/blob/main/parafold/create_fakemsa.py
@@ -30,9 +28,6 @@ class AlphaFold(Predictor):
         )
         self.db_preset = db_preset  # { 'reduced_dbs', 'full_dbs' }
         self.output_dir = output_dir
-        if output_dir is None:
-            workspace = Workspace.instance()
-            self._output_dir = f"{workspace.root_dir}/alphafold_outputs"
 
     def predict_pdb_str(self, sequence: str) -> str:
         pdb_path = ".aftemp.pdb"
@@ -43,10 +38,9 @@ class AlphaFold(Predictor):
         return prediction
 
     def predict_pdb_file(self, sequence: str, pdb_path: str) -> None:
-        os.makedirs(self._output_dir, exist_ok=True)
+        os.makedirs(self.output_dir, exist_ok=True)
         protein_name = os.path.splitext(os.path.basename(pdb_path))[0]
-        workspace = Workspace.instance()
-        fasta_path = f"{workspace.root_dir}/{protein_name}.fasta"
+        fasta_path = f"{self.output_dir}/{protein_name}.fasta"
         with open(fasta_path, "wt", encoding="utf-8") as fasta_file:
             fasta_file.write(f">{protein_name}\n{sequence}\n")
         # run the script for creating an empty MSA
@@ -55,12 +49,12 @@ class AlphaFold(Predictor):
                 "python3",
                 self.path_to_create_fakemsa_py,
                 f"--fasta_paths={fasta_path}",
-                f"--output_dir={self._output_dir}",
+                f"--output_dir={self.output_dir}",
             ]
         )
         for line in self.run_alphafold_docker(fasta_path):
             print(line, end="")
-        prediction_pdb = f"{self._output_dir}/{protein_name}/ranked_0.pdb"
+        prediction_pdb = f"{self.output_dir}/{protein_name}/ranked_0.pdb"
         shutil.copyfile(prediction_pdb, pdb_path)
         os.remove(fasta_path)
 
@@ -73,7 +67,7 @@ class AlphaFold(Predictor):
             f"--max_template_date={self.max_template_date}",
             f"--model_preset={self.model_preset}",
             f"--db_preset={self.db_preset}",
-            f"--output_dir={self._output_dir}",
+            f"--output_dir={self.output_dir}",
             f"--mgnify_database_path={self.mgnify_database_path}",
             f"--data_dir={self.data_dir}",
         ]
