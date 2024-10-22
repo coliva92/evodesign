b24d829add6490c1bbe4c23421c46f21b4ab8d02
Carlos Oliva <olivam@protonmail.com>
2024-10-22 11:46:41 -0300

    Cambio de estilo en el código en Exceptions y RetrievableSettings

--- evodesign/Exceptions.py
+++ evodesign/Exceptions.py
@@ -2,20 +2,16 @@ class HttpBadRequest(RuntimeError):
     pass
 
 
-
 class HttpForbidden(RuntimeError):
     pass
-
 
 
 class HttpInternalServerError(RuntimeError):
     pass
 
 
-
 class HttpGatewayTimeout(RuntimeError):
     pass
-
 
 
 class HttpUnknownError(RuntimeError):
--- evodesign/RetrievableSettings.py
+++ evodesign/RetrievableSettings.py
@@ -1,22 +1,17 @@
 from abc import ABC
 
 
-
-
-
 class RetrievableSettings(ABC):
 
     def _class_name(self) -> str:
         class_name = self.__class__.__qualname__
         module_name = self.__class__.__module__
-        result = f'{module_name}.{class_name}'
-        if module_name == 'builtins': # Ignore 'builtins.' module name
+        result = f"{module_name}.{class_name}"
+        if module_name == "builtins":  # Ignore 'builtins.' module name
             result = class_name
-        i = result.find('.') # Remove 'evodesign.' at the beginning
-        j = result.rfind('.') # Remove repeated class name at the end
+        i = result.find(".")  # Remove 'evodesign.' at the beginning
+        j = result.rfind(".")  # Remove repeated class name at the end
         return result[i + 1 : j]
-  
-
 
     def settings(self) -> dict:
         # returns all the public instance and class attributes as a dictionary
@@ -25,6 +20,6 @@ class RetrievableSettings(ABC):
             if isinstance(value, RetrievableSettings):
                 # apply the function recursively
                 settings_dict[key] = value.settings()
-            elif not key.startswith('_'):
+            elif not key.startswith("_"):
                 settings_dict[key] = value
-        return { self._class_name(): settings_dict }
+        return {self._class_name(): settings_dict}
