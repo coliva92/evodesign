80c7b4c21dbebe9f84eaf086f3984ee55e82c224
Carlos Oliva <olivam@protonmail.com>
2025-03-11 19:44:35 -0700

    Refactoricé Metrics.RosettaEnergyFunction

--- evodesign/Metrics/RosettaEnergyFunction.py
+++ evodesign/Metrics/RosettaEnergyFunction.py
@@ -1,39 +1,14 @@
 from .Metric import Metric
-from typing import Optional, List
-from ..Context import Context
-from Bio.PDB.Atom import Atom
-import pandas as pd
 import pyrosetta
-
-
-
 
 
 class RosettaEnergyFunction(Metric):
 
-  def __init__(self, column: Optional[str] = None) -> None:
-    super().__init__(column)
-    self._score_fn = None
-  
+    _score_fn = None
 
-
-  def _compute_values(self, 
-                      backbone: List[Atom],
-                      data: pd.Series,
-                      context: Context
-                      ) -> pd.Series:
-    pdb_path = f'{context.workspace.pdbs_dir}/prot_{data["sequence_id"]}.pdb'
-    score = self._ref2015(pdb_path)
-    data[self.column_name()] = score
-    return data
-  
-
-
-  def _ref2015(self, pdb_path: str) -> float:
-    if self._score_fn is None:
-      pyrosetta.init()
-      self._score_fn = pyrosetta.get_score_function(True)
-    pose = pyrosetta.pose_from_pdb(pdb_path)
-    score = self._score_fn(pose)
-    return score
-  
+    def do(self, pdb_path: str, **kwargs) -> float:
+        if self._score_fn is None:
+            pyrosetta.init()
+            self._score_fn = pyrosetta.get_score_function(True)
+        pose = pyrosetta.pose_from_pdb(pdb_path)
+        return self._score_fn(pose)
