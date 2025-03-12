d6e264174a26752da01fc81d517735dfde02a1fc
Carlos Oliva <olivam@protonmail.com>
2025-03-11 22:29:22 -0700

    Refactoricé Fitness.WeightedMean para que use los Metrics refactorizados

--- evodesign/Fitness/WeightedMean.py
+++ evodesign/Fitness/WeightedMean.py
@@ -4,19 +4,15 @@ from typing import List
 import numpy.typing as npt
 from Bio.PDB.Structure import Structure
 from Bio.PDB.Atom import Atom
+from ..Metrics.RMSD import RMSD
+from ..Metrics.GDT import GDT
+from ..Metrics.TMScore import TMScore
+from ..Metrics.DistanceMap import DistanceMap
+from ..Metrics.Cyclization import Cyclization
+from ..Metrics.RosettaEnergyFunction import RosettaEnergyFunction
 
 
 class WeightedMean(FitnessFunction):
-
-    _terms = {
-        "rmsd_weight": 0,
-        "gdt_weight": 1,
-        "tm_score_weight": 2,
-        "distance_map_weight": 3,
-        "cyclization_weight": 4,
-        "rosetta_energy_weight": 5,
-        "plddt_weight": 6,
-    }
 
     def __init__(
         self,
@@ -28,6 +24,12 @@ class WeightedMean(FitnessFunction):
         cyclization_weight: float = 1.0,
         rosetta_energy_weight: float = 1.0,
         plddt_weight: float = 1.0,
+        rmsd_calc: RMSD = RMSD(),
+        gdt_calc: GDT = GDT(),
+        tm_score_calc: TMScore = TMScore(),
+        dm_calc: DistanceMap = DistanceMap(),
+        cyc_calc: Cyclization = Cyclization(),
+        eng_calc: RosettaEnergyFunction = RosettaEnergyFunction(),
     ) -> None:
         super().__init__(upper_bound)
         self.rmsd_weight = rmsd_weight
@@ -37,16 +39,14 @@ class WeightedMean(FitnessFunction):
         self.cyclization_weight = cyclization_weight
         self.rosetta_energy_weight = rosetta_energy_weight
         self.plddt_weight = plddt_weight
+        self.rmsd_calc = rmsd_calc
+        self.gdt_calc = gdt_calc
+        self.tm_score_calc = tm_score_calc
+        self.dm_calc = dm_calc
+        self.cyc_calc = cyc_calc
+        self.eng_calc = eng_calc
         self._weights = np.array(
-            [
-                self.rmsd_weight,
-                self.gdt_weight,
-                self.tm_score_weight,
-                self.distance_map_weight,
-                self.cyclization_weight,
-                self.rosetta_energy_weight,
-                self.plddt_weight,
-            ]
+            [weight for name, weight in self.__dict__.items() if "_weight" in name]
         )
 
     def compute_term_values(
@@ -60,24 +60,14 @@ class WeightedMean(FitnessFunction):
         ref_backbone: List[Atom],
         ref_ca_atoms: List[Atom],
     ) -> npt.NDArray[np.float64]:
-        # TODO después de refactorizar los metrics, incluirlos aquí
+        kwargs = locals()
         return np.array(
-            [rmsd, gdt, tm_score, distance_map_rms, cyc_z_score, energy_norm, plddt]
+            [
+                calc.do(**kwargs)
+                for name, calc in self.__dict__.items()
+                if "_calc" in name
+            ]
         )
 
     def compute_fitness(self, term_values: npt.NDArray[np.float64]) -> float:
         return np.average(term_values, weights=self._weights)
-
-    def set_weight(self, term_name: str, weight: float) -> None:
-        self._weights[self._terms[term_name]] = self.__dict__[term_name] = weight
-        self._weights = np.array(
-            [
-                self.rmsd_weight,
-                self.gdt_weight,
-                self.tm_score_weight,
-                self.distance_map_weight,
-                self.cyclization_weight,
-                self.rosetta_energy_weight,
-                self.plddt_weight,
-            ]
-        )
