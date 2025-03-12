b191e2ce91df6915c15fde23556039626a09f2ee
Carlos Oliva <olivam@protonmail.com>
2025-03-11 19:48:14 -0700

    Refactoricé Metrics.ESM2DescriptorsRms y lo renombré Metrics.ESM2Descriptors

--- evodesign/Metrics/ESM2Descriptors.py
+++ evodesign/Metrics/ESM2Descriptors.py
@@ -1,115 +1,74 @@
 from .Metric import Metric
-from ..Workspace import Workspace
 import numpy as np
 import numpy.typing as npt
-import os
-from typing import Optional, List, Dict
-from ..Context import Context
-from Bio.PDB.Atom import Atom
-import pandas as pd
-
-
-
+from typing import Optional
 
 
 class ESM2Descriptors(Metric):
 
-  model = None
-  batch_converter = None
-  
+    _model = None
+    _batch_converter = None
 
+    def __init__(self, gpu_device: Optional[str] = "cuda:0") -> None:
+        super().__init__()
+        self.gpu_device = gpu_device
 
-  def _params(self) -> dict:
-    params = super()._params()
-    params['gpu_device'] = self._gpu_device
-    return params
-  
+    def do(
+        self,
+        model_sequence: str,
+        ref_desc_matrix: npt.NDArray[np.float64],
+        **kwargs
+    ) -> float:
+        # compute the RMS of each residue
+        model_desc_matrix = self.compute_descriptors_matrix(model_sequence)
+        rms = np.array(
+            [
+                np.sqrt(np.mean((ref_desc_matrix[i] - model_desc_matrix[i]) ** 2))
+                for i in range(ref_desc_matrix.shape[0])
+            ]
+        )
 
+        # then compute the average of the RMS of all residues; this is the same as computing
+        # the weighted mean since all residues have the same number of descriptors
+        return np.mean(rms)
 
-  def __init__(self, 
-               gpu_device: Optional[str] = "cuda:0",
-               column: Optional[str] = None
-               ) -> None:
-    super().__init__(column)
-    self._gpu_device = gpu_device
-  
+    def compute_descriptors_matrix(
+        self, sequence: str, sequence_name: str = "temp_protein"
+    ) -> npt.NDArray[np.float64]:
+        # initialize the model if not yet initialized
+        import torch
 
+        if self._model is None or self._batch_converter is None:
+            import esm
 
-  def _compute_values(self, 
-                      backbone: List[Atom],
-                      data: pd.Series,
-                      context: Context
-                      ) -> pd.Series:
-    sequence, sequence_id = data['sequence'], data['sequence_id']
-    vectors = self.compute_descriptor_vectors(sequence, sequence_id)
-    txt_path = f'{context.workspace.esm2_dir}/{sequence_id}.txt'
-    self.save_descriptor_vectors_txt(vectors, txt_path)
-    data[self.column_name()] = txt_path
-    return data
-  
+            self._model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+            self._batch_converter = alphabet.get_batch_converter()
+            self._model.eval()
+            if torch.cuda.is_available() and self.gpu_device is not None:
+                device = torch.device(self.gpu_device)
+                self._model = self._model.to(device)
+        data = [(sequence_name, sequence)]
+        seq_ids, seqs, tokens = self._batch_converter(data)
+        if torch.cuda.is_available() and self.gpu_device is not None:
+            tokens = tokens.to(device=self.gpu_device, non_blocking=True)
+        with torch.no_grad():
+            result = self._model(
+                tokens, repr_layers=[self._model.num_layers], return_contacts=False
+            )
 
+        # `result['representations']` contains the weights of each layer in the
+        # neural net; we only want the weights of the last layer
+        last_layer = result["representations"][self._model.num_layers]
 
-  def compute_descriptor_vectors(self, 
-                                 sequence: str,
-                                 sequence_id: str = 'prot_0000_0000'
-                                 ) -> npt.NDArray[np.float64]:
-    # initialize the model if not yet initialized
-    import torch
-    if self.model is None or self.batch_converter is None:
-      import esm
-      self.model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-      self.batch_converter = alphabet.get_batch_converter()
-      self.model.eval()
-      if torch.cuda.is_available() and self._gpu_device is not None:
-        device = torch.device(self._gpu_device)
-        self.model = self.model.to(device)
-    
-    data = [( sequence_id, sequence )]
-    seq_ids, seqs, tokens = self.batch_converter(data)
-    if torch.cuda.is_available() and self._gpu_device is not None:
-      tokens = tokens.to(device='cuda', non_blocking=True)
-    with torch.no_grad():
-      result = self.model(tokens, 
-                          repr_layers=[self.model.num_layers], 
-                          return_contacts=False)
-    
-    # `result['representations']` contains the weights of each layer in the 
-    # NN; we only want the weights of the last layer
-    last_layer = result['representations'][self.model.num_layers]
+        # the last layer contains a certain number of weights per token; a token is
+        # an integer representation of each AA in the input sequence, however,
+        # additional tokens are appended at the beginning and at the end of said
+        # sequence; we only want to retrieve the weights corresponding to the AA
+        # in the sequence
+        matrix = last_layer[0][1 : len(seqs[0]) + 1].cpu().numpy()
 
-    # the last layer contains a certain number of weights per token; a token is
-    # an integer representation of each AA in the input sequence, however, 
-    # additional tokens are appended at the beginning and at the end of said 
-    # sequence; we only want to retrieve the weights corresponding to the AA
-    # in the sequence
-    matrix = last_layer[0][1 : len(seqs[0]) + 1].cpu().numpy()
+        # free GPU memory
+        del tokens
+        del result
 
-    # free GPU memory
-    del tokens
-    del result
-
-    return matrix
-  
-
-
-  @classmethod
-  def save_descriptor_vectors_txt(cls, 
-                                  vectors: npt.NDArray[np.float64], 
-                                  txt_path: str
-                                  ) -> None:
-    os.makedirs(os.path.dirname(os.path.abspath(txt_path)), exist_ok=True)
-    with open(txt_path, 'wt', encoding='utf-8') as txt_file:
-      for i in range(vectors.shape[0]):
-        vector = vectors[i].tolist()
-        txt_file.write(','.join(map(str, vector)) + '\n')
-
-  
-
-  @classmethod
-  def load_descriptor_vectors_txt(cls, txt_path: str) -> npt.NDArray[np.float64]:
-    vectors = np.array([
-      [ float(value.strip()) for value in line.split(',') ]
-      for line in open(txt_path, 'rt', encoding='utf-8')
-    ])
-    return vectors
-  
+        return matrix
