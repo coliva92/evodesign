d8f0bf5dbd8be7c95cd59296617772c927104fc7
Carlos Oliva <olivam@protonmail.com>
2025-10-24 15:51:07 -0700

    Modifiqué local_api_esm2_only para que retorne también el mapa de contactos

--- gunicorn_scripts/local_api_esm2_only.py
+++ gunicorn_scripts/local_api_esm2_only.py
@@ -3,11 +3,11 @@ gunicorn -w 1 -b 127.0.0.1:8000 local_api_esm_only:app
 """
 
 from flask import Flask, request, jsonify
-from Metrics.ESM2Descriptors import ESM2Descriptors
+from Metrics.ESM2 import ESM2
 
 
-esm_model = ESM2Descriptors(gpu_device="cuda:0")
-esm_model.compute_descriptors_matrix("GREETINGS")
+esm_model = ESM2(gpu_device="cuda:0")
+esm_model.query_model("GREETINGS")
 
 
 app = Flask(__name__)
@@ -17,8 +17,13 @@ app = Flask(__name__)
 def esm():
     data = request.get_json()
     sequence = data["sequence"]
-    matrix = esm_model.compute_descriptors_matrix(sequence)
-    return jsonify({"descriptors": matrix.tolist()})
+    desc_matrix, predicted_contacts = esm_model.query_model(sequence)
+    return jsonify(
+        {
+            "desc_matrix": desc_matrix.tolist(),
+            "predicted_contacts": predicted_contacts.tolist(),
+        }
+    )
 
 
 if __name__ == "__main__":
