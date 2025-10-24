9dbdd53da92da5167100017dffcbf503b9a493e1
Carlos Oliva <olivam@protonmail.com>
2025-10-24 15:50:24 -0700

    Renombré Metrics.ESM2DescriptorsRemoteAPI y lo modifiqué para que retorne también el mapa de contactos.

---
+++ evodesign/Metrics/ESM2RemoteAPI.py
@@ -0,0 +1,31 @@
+from .ESM2 import ESM2
+from ..Utils.APIRequester import APIRequester
+import numpy as np
+import numpy.typing as npt
+from typing import Tuple
+
+
+class ESM2RemoteAPI(ESM2):
+
+    def __init__(
+        self,
+        requester: APIRequester = APIRequester(
+            url="http://127.0.0.1:8000/esm",
+            json_request_key="sequence",
+            json_response_key="results",
+        ),
+    ) -> None:
+        super().__init__(
+            gpu_device=None,
+        )
+        self.requester = requester
+
+    def query_model(
+        self,
+        sequence: str,
+        sequence_name: str = "tmp_protein",
+    ) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        data = self.requester.post(sequence)
+        desc_matrix = np.array(data["desc_matrix"])
+        predicted_contacts = np.array(data["predicted_contacts"])
+        return desc_matrix, predicted_contacts
