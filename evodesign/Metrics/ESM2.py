331c5855cb2b85af5312a28cb1c827e183f69ab6
Carlos Oliva <olivam@protonmail.com>
2025-10-24 15:49:29 -0700

    Modifiqué la clase Metrics.ESM2Descriptors para que el modelo se calcule por separado en Metrics.ESM2.

---
+++ evodesign/Metrics/ESM2.py
@@ -0,0 +1,64 @@
+from ..RetrievableSettings import RetrievableSettings
+from typing import Optional, Tuple
+import numpy as np
+import numpy.typing as npt
+
+
+class ESM2(RetrievableSettings):
+
+    _model = None
+    _batch_converter = None
+
+    def __init__(
+        self,
+        gpu_device: Optional[str] = "cuda:0",
+    ) -> None:
+        self.gpu_device = gpu_device
+
+    def query_model(
+        self,
+        sequence: str,
+        sequence_name: str = "tmp_protein",
+    ) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        # initialize the model if not yet initialized
+        import torch
+
+        if self._model is None or self._batch_converter is None:
+            import esm
+
+            self._model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+            self._batch_converter = alphabet.get_batch_converter()
+            self._model.eval()
+            if torch.cuda.is_available() and self.gpu_device is not None:
+                device = torch.device(self.gpu_device)
+                self._model = self._model.to(device)
+        data = [(sequence_name, sequence)]
+        seq_ids, seqs, tokens = self._batch_converter(data)
+        if torch.cuda.is_available() and self.gpu_device is not None:
+            tokens = tokens.to(device=self.gpu_device, non_blocking=True)
+        with torch.no_grad():
+            result = self._model(
+                tokens, repr_layers=[self._model.num_layers], return_contacts=True
+            )
+
+        # `result['representations']` contains the weights of each layer in the
+        # neural net; we only want the weights of the last layer
+        last_layer = result["representations"][self._model.num_layers]
+
+        # the last layer contains a certain number of weights per token; a token is
+        # an integer representation of each AA in the input sequence, however,
+        # additional tokens are appended at the beginning and at the end of said
+        # sequence; we only want to retrieve the weights corresponding to the AA
+        # in the sequence
+        desc_matrix = last_layer[0][1 : len(seqs[0]) + 1].cpu().numpy()
+
+        predicted_contacts_matrix = result["contacts"][0].cpu().numpy()
+        np.fill_diagonal(predicted_contacts_matrix, 1.0)
+        row_idx, col_idx = np.triu_indices_from(predicted_contacts_matrix, k=0)
+        predicted_contacts = predicted_contacts[row_idx, col_idx]
+
+        # free GPU memory
+        del tokens
+        del result
+
+        return desc_matrix, predicted_contacts
--- evodesign/Metrics/ESM2Descriptors.py
+++ evodesign/Metrics/ESM2Descriptors.py
@@ -1,5 +1,6 @@
 from .Metric import Metric
 from .ContextInterface import ContextInterface
+from .ESM2 import ESM2
 import numpy as np
 import numpy.typing as npt
 from typing import Optional, Dict, Tuple
@@ -9,96 +10,58 @@ from .Normalization.Reciprocal import Reciprocal
 
 class ESM2Descriptors(Metric):
 
-    _model = None
-    _batch_converter = None
-
     def __init__(
         self,
-        gpu_device: Optional[str] = "cuda:0",
+        esm_model: ESM2 = ESM2(),
         normalization: Optional[Normalization] = Reciprocal(),
     ) -> None:
         super().__init__()
-        self.gpu_device = gpu_device
+        self.esm_model = esm_model
         self.normalization = normalization
 
     def do(
         self,
-        model_sequence: str,
+        model_desc_matrix: npt.NDArray[np.float64],
         ref_desc_matrix: npt.NDArray[np.float64],
         **kwargs,
     ) -> Tuple[float, Optional[float]]:
-        # compute the RMS of each residue
-        model_desc_matrix = self.compute_descriptors_matrix(model_sequence)
-        rmse = np.array(
+        # compute the squared error of each residue
+        error = np.array(
             [
                 np.sqrt(np.mean((ref_desc_matrix[i] - model_desc_matrix[i]) ** 2))
                 for i in range(ref_desc_matrix.shape[0])
             ]
         )
 
-        # then compute the average of the RMS of all residues; this is the same as computing
+        # then compute average over all residues; this is the same as computing
         # the weighted mean since all residues have the same number of descriptors
-        rmse = np.mean(rmse)
+        rmse = np.mean(error)
+
         norm = None
         if self.normalization is not None:
             norm = self.normalization.do(rmse)
+        
         return rmse, norm
 
     def do_for_fitness_fn(
         self,
         context: ContextInterface,
     ) -> Dict[str, float]:
-        model_sequence = context.get_model_chain().sequence
-        ref_desc_matrix = context.get_extra_param_value("reference_esm2_descriptors")
+        ref_desc_matrix = context.get_extra_param_value("esm2_ref_desc_matrix")
         if ref_desc_matrix is None:
             ref_sequence = context.get_reference_chain().sequence
-            ref_desc_matrix = self.compute_descriptors_matrix(ref_sequence)
-            context.set_extra_param_value("reference_esm2_descriptors", ref_desc_matrix)
-        rmse, norm = self.do(model_sequence, ref_desc_matrix)
+            ref_desc_matrix, _ = self.esm_model.query_model(ref_sequence)
+            context.set_extra_param_value("esm2_ref_desc_matrix", ref_desc_matrix)
+        model_desc_matrix = context.get_extra_param_value("esm2_model_desc_matrix")
+        if model_desc_matrix is None:
+            model_sequence = context.get_model_chain().sequence
+            model_desc_matrix, model_contact_map = self.esm_model.query_model(
+                model_sequence
+            )
+            context.set_extra_param_value("esm2_model_desc_matrix", model_desc_matrix)
+            context.set_extra_param_value("esm2_predicted_contacts", model_contact_map)
+        rmse, norm = self.do(model_desc_matrix, ref_desc_matrix)
         return {
             "rmse": rmse,
             "norm_rmse": norm,
         }
-
-    def compute_descriptors_matrix(
-        self,
-        sequence: str,
-        sequence_name: str = "tmp_protein",
-    ) -> npt.NDArray[np.float64]:
-        # initialize the model if not yet initialized
-        import torch
-
-        if self._model is None or self._batch_converter is None:
-            import esm
-
-            self._model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-            self._batch_converter = alphabet.get_batch_converter()
-            self._model.eval()
-            if torch.cuda.is_available() and self.gpu_device is not None:
-                device = torch.device(self.gpu_device)
-                self._model = self._model.to(device)
-        data = [(sequence_name, sequence)]
-        seq_ids, seqs, tokens = self._batch_converter(data)
-        if torch.cuda.is_available() and self.gpu_device is not None:
-            tokens = tokens.to(device=self.gpu_device, non_blocking=True)
-        with torch.no_grad():
-            result = self._model(
-                tokens, repr_layers=[self._model.num_layers], return_contacts=False
-            )
-
-        # `result['representations']` contains the weights of each layer in the
-        # neural net; we only want the weights of the last layer
-        last_layer = result["representations"][self._model.num_layers]
-
-        # the last layer contains a certain number of weights per token; a token is
-        # an integer representation of each AA in the input sequence, however,
-        # additional tokens are appended at the beginning and at the end of said
-        # sequence; we only want to retrieve the weights corresponding to the AA
-        # in the sequence
-        matrix = last_layer[0][1 : len(seqs[0]) + 1].cpu().numpy()
-
-        # free GPU memory
-        del tokens
-        del result
-
-        return matrix
