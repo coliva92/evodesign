bfbf29cb7c385dad2b6888ce9a3a386b880af91e
Carlos Oliva <olivam@protonmail.com>
2025-11-27 17:43:13 -0800

    Separé las clases Utils.Chain y Utils.ChainFactory para permitir Chain vacíos

--- evodesign/Utils/Chain.py
+++ evodesign/Utils/Chain.py
@@ -1,79 +1,19 @@
+from dataclasses import dataclass
 from typing import List, Optional
 from Bio.PDB.Structure import Structure
 from Bio.PDB.Atom import Atom
-from Bio.PDB import PDBParser, PPBuilder
 import numpy as np
 import numpy.typing as npt
-import os
 
 
-def numpy_sequence_to_str(sequence_numpy: npt.NDArray[np.int64]):
-    return "".join(Chain.AMINO_ACIDS[i] for i in sequence_numpy)
-
-
+@dataclass
 class Chain:
 
-    _ppb = None
-    BACKBONE_ATOMS = dict.fromkeys(["N", "CA", "C", "O"])
-    AMINO_ACIDS = list("ACDEFGHIKLMNPQRSTVWY")
-    MAP_AMINO_ACID_TO_INT = {aa: i for i, aa in enumerate(AMINO_ACIDS)}
-
-    def __init__(
-        self,
-        structure: Structure,
-        model_id: int = 0,
-        chain_id: str = "A",
-        pdb_path: Optional[str] = None
-    ):
-        self.structure = structure
-        self.model_id = model_id
-        self.chain_id = chain_id
-        self.pdb_path = pdb_path
-        self.sequence = self._get_sequence()
-        self.sequence_numpy = np.array(
-            [self.MAP_AMINO_ACID_TO_INT[aa] for aa in self.sequence], dtype=np.int64
-        )
-        self.backbone_atoms = self._get_backbone_atoms()
-        self.ca_atoms = self._get_ca_atoms()
-
-    def _get_sequence(self) -> str:
-        if self._ppb is None:
-            self._ppb = PPBuilder()
-        chain = self.structure[self.model_id][self.chain_id]
-        return "".join(str(pp.get_sequence()) for pp in self._ppb.build_peptides(chain))
-
-    def _get_backbone_atoms(self) -> List[Atom]:
-        return [
-            atom
-            for atom in self.structure[self.model_id][self.chain_id].get_atoms()
-            if atom.get_name() in self.BACKBONE_ATOMS
-        ]
-
-    def _get_ca_atoms(self) -> List[Atom]:
-        return [
-            atom
-            for atom in self.structure[self.model_id][self.chain_id].get_atoms()
-            if atom.get_name() == "CA"
-        ]
-
-
-class ChainFactory:
-
-    _parser = None
-
-    @classmethod
-    def create(
-        cls,
-        pdb_path: str,
-        model_id: int = 0,
-        chain_id: Optional[str] = None,
-    ) -> Chain:
-        if cls._parser is None:
-            cls._parser = PDBParser(QUIET=True)
-        structure_id = os.path.splitext(os.path.basename(pdb_path))[0]
-        structure = cls._parser.get_structure(structure_id, pdb_path)
-        if chain_id is None:
-            for chain_id in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-                if chain_id in structure[model_id]:
-                    break
-        return Chain(structure, model_id, chain_id, pdb_path)
+    sequence: Optional[str] = None
+    sequence_numpy: Optional[npt.NDArray[np.float64]]
+    structure: Optional[Structure] = None
+    model_id: Optional[int] = None
+    chain_id: Optional[str] = None
+    pdb_path: Optional[str] = None
+    backbone_atoms: Optional[List[Atom]] = None
+    ca_atoms: Optional[List[Atom]] = None
---
+++ evodesign/Utils/ChainFactory.py
@@ -0,0 +1,87 @@
+from typing import List, Optional
+from .Chain import Chain
+from Bio.PDB.Structure import Structure
+from Bio.PDB.Atom import Atom
+from Bio.PDB import PDBParser
+from Bio.PDB.Polypeptide import three_to_one, is_aa
+import numpy as np
+import numpy.typing as npt
+import os
+
+
+class ChainFactory:
+
+    _parser = None
+    BACKBONE_ATOMS = dict.fromkeys(["N", "CA", "C", "O"])
+    AMINO_ACIDS = list("ACDEFGHIKLMNPQRSTVWY")
+    MAP_AMINO_ACID_TO_INT = {aa: i for i, aa in enumerate(AMINO_ACIDS)}
+
+    @classmethod
+    def create_from_pdb(
+        cls,
+        pdb_path: str,
+        model_id: int = 0,
+        chain_id: Optional[str] = None,
+    ) -> Chain:
+        if cls._parser is None:
+            cls._parser = PDBParser(QUIET=True)
+        structure_id = os.path.splitext(os.path.basename(pdb_path))[0]
+        structure = cls._parser.get_structure(structure_id, pdb_path)
+        if chain_id is None:
+            for chain_id in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                if chain_id in structure[model_id]:
+                    break
+        sequence = cls.get_sequence(structure, model_id, chain_id)
+        sequence_numpy = cls.sequence_numpy_to_str(sequence)
+        backbone_atoms = cls.get_backbone_atoms(structure, model_id, chain_id)
+        ca_atoms = cls.get_ca_atoms(structure, model_id, chain_id)
+        return Chain(
+            sequence,
+            sequence_numpy,
+            structure,
+            model_id,
+            chain_id,
+            pdb_path,
+            backbone_atoms,
+            ca_atoms,
+        )
+
+    @classmethod
+    def create_from_sequence(cls, sequence: str) -> Chain:
+        sequence_numpy = cls.sequence_str_to_numpy(sequence)
+        return Chain(sequence=sequence, sequence_numpy=sequence_numpy)
+
+    @classmethod
+    def get_sequence(cls, structure: Structure, model_id: int, chain_id: str) -> str:
+        chain = structure[model_id][chain_id]
+        return "".join(three_to_one(r.get_resname()) for r in chain if is_aa(r))
+
+    @classmethod
+    def get_backbone_atoms(
+        cls, structure: Structure, model_id: int, chain_id: str
+    ) -> List[Atom]:
+        return [
+            atom
+            for atom in structure[model_id][chain_id].get_atoms()
+            if atom.get_name() in cls.BACKBONE_ATOMS
+        ]
+
+    @classmethod
+    def get_ca_atoms(
+        cls, structure: Structure, model_id: int, chain_id: str
+    ) -> List[Atom]:
+        return [
+            atom
+            for atom in structure[model_id][chain_id].get_atoms()
+            if atom.get_name() == "CA"
+        ]
+
+    @classmethod
+    def sequence_str_to_numpy(cls, sequence: str) -> npt.NDArray[np.int64]:
+        return np.array(
+            [cls.MAP_AMINO_ACID_TO_INT[aa] for aa in sequence], dtype=np.int64
+        )
+
+    @classmethod
+    def sequence_numpy_to_str(cls, sequence_numpy: npt.NDArray[np.int64]) -> str:
+        return "".join(Chain.AMINO_ACIDS[i] for i in sequence_numpy)
