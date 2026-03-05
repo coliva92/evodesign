e071260d72f33038cc3211e220912b997bca21ce
Carlos Oliva <olivam@protonmail.com>
2026-03-04 17:24:25 -0800

    Corregí `Callbacks.PopulationRestarter` porque no estaba encadenando correctamente el siguiente callback

--- evodesign/Callbacks/PopulationRestarter.py
+++ evodesign/Callbacks/PopulationRestarter.py
@@ -6,16 +6,22 @@ from ..Statistics import (
     get_population_identity,
     ALPHABET_SIZE,
 )
+from typing import Optional
 import numpy as np
 
 
 class PopulationRestarter(Callback):
 
-    def __init__(self, diversity_loss_tol: float = 0.95) -> None:
+    def __init__(
+        self,
+        diversity_loss_tol: float = 0.95,
+        next_callback: Optional[Callback] = None,
+    ) -> None:
         super().__init__()
+        self.next_callback = next_callback
         self.diversity_loss_tol = diversity_loss_tol
         return
-    
+
     def notify(self, algorithm: PyMOOAlgorithm) -> None:
         population = np.array([solution for solution in algorithm.pop.get("X")])
         d1 = get_population_amino_acid_loss(population) / ALPHABET_SIZE
@@ -30,15 +36,11 @@ class PopulationRestarter(Callback):
             n_pop = len(algorithm.pop)
             n_new = n_pop - 1
             new_pop = algorithm.initialization.sampling(
-                algorithm.problem, 
-                n_new, 
-                algorithm=algorithm
+                algorithm.problem, n_new, algorithm=algorithm
             )
             # we must evaluate the individuals before returning to the algorithm
-            algorithm.evaluator.eval(
-                algorithm.problem, 
-                new_pop, 
-                algorithm=algorithm
-            )
+            algorithm.evaluator.eval(algorithm.problem, new_pop, algorithm=algorithm)
             algorithm.pop = Population.merge(elite, new_pop)
+        if self.next_callback is not None:
+            self.next_callback.notify(algorithm)
         return
