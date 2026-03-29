92705a1720a5e27a9b873901796e0f9d340c48fa
Carlos Oliva <olivam@protonmail.com>
2026-03-29 16:25:03 -0700

    Modifiqué `Metrics.ESM2ContactMapPredictedRef` para no escalar los valores del mapa de contactos

--- evodesign/Metrics/ESM2ContactMapPredictedRef.py
+++ evodesign/Metrics/ESM2ContactMapPredictedRef.py
@@ -14,12 +14,12 @@ class ESM2ContactMapPredictedRef(NonStructuralMetric):
         self,
         esm_model: ESM2 = ESM2(),
         submap_indices: Optional[List[int]] = None,
-        regulariaztion: Optional[Normalization] = None,
+        regularization: Optional[Normalization] = None,
     ) -> None:
         super().__init__()
         self.esm_model = esm_model
         self.submap_indices = submap_indices
-        self.regularization = regulariaztion
+        self.regularization = regularization
         return
 
     def do(
@@ -39,22 +39,22 @@ class ESM2ContactMapPredictedRef(NonStructuralMetric):
         context: ContextInterface,
     ) -> Dict[str, float]:
         ref_contact_map = context.get_extra_param_value("esm2_ref_contact_map")
-        scaling_factor = context.get_extra_param_value(
-            "esm2_contact_map_scaling_factor"
-        )
-        if ref_contact_map is None or scaling_factor is None:
+        # scaling_factor = context.get_extra_param_value(
+        #     "esm2_contact_map_scaling_factor"
+        # )
+        if ref_contact_map is None: # or scaling_factor is None:
             ref_sequence = context.get_reference_chain().sequence
             _, ref_contact_map = self.esm_model.query_model(
                 ref_sequence, submap_indices=self.submap_indices
             )
             a, b = np.min(ref_contact_map), np.max(ref_contact_map)
-            scaling_factor = 1 / (b - a)
+            # scaling_factor = 1 / (b - a)
             context.set_extra_param_value(
-                "esm2_ref_contact_map", ref_contact_map / scaling_factor
+                "esm2_ref_contact_map", ref_contact_map #/ scaling_factor
             )
-            context.set_extra_param_value(
-                "esm2_contact_map_scaling_factor", scaling_factor
-            )
+            # context.set_extra_param_value(
+            #     "esm2_contact_map_scaling_factor", scaling_factor
+            # )
         predicted_contacts = context.get_extra_param_value("esm2_predicted_contacts")
         if predicted_contacts is None:
             model_sequence = context.get_model_chain().sequence
@@ -63,7 +63,7 @@ class ESM2ContactMapPredictedRef(NonStructuralMetric):
             )
             context.set_extra_param_value("esm2_model_desc_matrix", model_desc_matrix)
             context.set_extra_param_value(
-                "esm2_predicted_contacts", predicted_contacts / scaling_factor
+                "esm2_predicted_contacts", predicted_contacts #/ scaling_factor
             )
         sim, reg_sim = self.do(predicted_contacts, ref_contact_map)
         return {"cos_similarity": sim, "reg_cos_similarity": reg_sim}
