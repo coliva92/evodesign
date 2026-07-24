a32f0f971cdd0200e19d47378f8f6252fd18a066
Jazmin <jazminamg26@gmail.com>
2026-07-24 12:04:17 -0700

    En Chemistry.Geometry agregué funciones para calcular el mapa de contactos y el mapa de distancias.

--- evodesign/Chemistry/Geometry.py
+++ evodesign/Chemistry/Geometry.py
@@ -1,6 +1,8 @@
 from Bio.PDB.Residue import Residue
 from Bio.PDB.Atom import Atom
 import numpy as np
+import numpy.typing as npt
+import itertools
 from typing import Optional, List
 
 
@@ -29,3 +31,27 @@ def compute_dihedral_angle(a: Atom, b: Atom, c: Atom, d: Atom) -> float:
     if proyection < 0.0:
         dihedral = -dihedral
     return dihedral
+
+
+def compute_distance_map(atoms: List[Atom]) -> npt.NDArray[np.float64]:
+    return np.array([a - b for (a, b) in itertools.combinations(atoms, 2)])
+
+
+def compute_contacts_map(atoms: List[Atom], 
+                         distance_threshold: float = 8.0,
+                         min_separation: int = 6,
+                         ) -> npt.NDArray[np.int64]:
+    L = len(atoms)
+    if L == 0:
+        raise ValueError
+    distances = compute_distance_map(atoms)
+    row_indices, column_indices = np.triu_indices(L, k=1)
+    separation_mask = (column_indices - row_indices) >= min_separation
+    contacts_mask = distances <= distance_threshold
+    contacts = separation_mask & contacts_mask
+    contacts_map = np.zeros((L, L), dtype=np.int64)
+    rows = row_indices[contacts]
+    columns = column_indices[contacts]
+    contacts_map[rows, columns] = 1
+    contacts_map[columns, rows] = 1 
+    return contacts_map
