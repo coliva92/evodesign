455a8bc516255c12055f0d5574d89fe6db6e9f0e
Jazmin <jazminamg26@gmail.com>
2026-07-27 16:22:15 -0700

    La versión anterior de Metrics.GDT ahora la moví a Metrics.Legacy.GDT

---
+++ evodesign/Metrics/Legacy/GDT.py
@@ -0,0 +1,44 @@
+from ..StructuralMetric import StructuralMetric
+from typing import List, Optional, Dict
+import numpy as np
+from Bio.PDB.Atom import Atom
+from Bio.PDB.Superimposer import Superimposer
+from ..ContextInterface import ContextInterface
+
+
+class v1(StructuralMetric):
+
+    def __init__(
+        self,
+        cutoffs: List[float] = [1.0, 2.0, 4.0, 8.0],
+    ) -> None:
+        super().__init__()
+        self.cutoffs = cutoffs
+
+    def do(
+        self,
+        model_backbone: List[Atom],
+        ref_backbone: List[Atom],
+        superimposer: Optional[Superimposer] = None,
+        **kwargs,
+    ) -> float:
+        # assume the backbones are superimposed if no superimposer is provided
+        if superimposer is not None:
+            superimposer.set_atoms(ref_backbone, model_backbone)
+            superimposer.apply(model_backbone)
+        distances = np.array([a - b for a, b in zip(model_backbone, ref_backbone)])
+        gdt = np.mean([np.mean([d <= c for d in distances]) for c in self.cutoffs])
+        return gdt
+
+    def do_for_fitness_fn(
+        self,
+        context: ContextInterface,
+    ) -> Dict[str, float]:
+        model_backbone = context.get_model_chain().backbone_atoms
+        ref_backbone = context.get_reference_chain().backbone_atoms
+        superimposer = context.get_extra_param_value("superimposer")
+        if superimposer is None:
+            superimposer = Superimposer()
+            context.set_extra_param_value("superimposer", superimposer)
+        gdt = self.do(model_backbone, ref_backbone, superimposer)
+        return {"gdt": gdt}
