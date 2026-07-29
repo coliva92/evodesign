bb556b902bb7aa45ff1de1d95c1a0b758d41d745
Carlos Oliva <carlosalberto@cicese.edu.mx>
2026-07-28 17:04:03 -0700

    fix (Metrics.Cyclization): fixed an import statement

--- evodesign/Metrics/Cyclization.py
+++ evodesign/Metrics/Cyclization.py
@@ -2,8 +2,7 @@ from typing import Dict, List, Tuple
 
 from Bio.PDB.Atom import Atom
 
-from Metrics.Normalization.Formulas import reciprocal
-from Metrics.Normalization.Formulas import z_score as compute_z
+from .Normalization.Formulas import reciprocal, z_score as compute_z
 
 from .ContextInterface import ContextInterface
 from .StructuralMetric import StructuralMetric
